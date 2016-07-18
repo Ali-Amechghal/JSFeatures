dd689a1b2d5ec23275fcf5750c7c3cfd072227f2
Ali-Amechghal <ali.amechghal@gmail.com>
2016-07-18 19:51:02 +0100

    arrays

---
+++ arrays.js
@@ -0,0 +1,16 @@
+(function(){
+
+	var arr = ['a', 'b' , ,'c'];
+	console.log(arr.length);
+	function getCount(arr){
+		if(!Array.isArray(arr))
+			throw new Error('is not an Array');
+
+		var count = 0;
+		arr.forEach(function(item){
+			count++
+		});
+		return count;
+	}
+	console.log(getCount(arr));
+}());	
