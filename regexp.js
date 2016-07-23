3a1ed4feb2fdc32ca85d1d2ebe4e695c94a0faeb
Ali-Amechghal <ali.amechghal@gmail.com>
2016-07-23 16:59:35 +0100

    regex groups

--- regexp.js
+++ regexp.js
@@ -4,5 +4,16 @@
 	var isValide = /[\s\S]/.test('\n');
 	console.log(isValide); //true
 
-	
+	//control date
+	function parseIsoDate(str_date){
+		var match =/^([0-9]{4})-([0-9]{2})-([0-9]{2})$/.exec(str_date);
+		if(!match)
+			throw new Error('Invalide Iso Date');
+
+		console.log('Year : '+match[1]);
+		console.log('Month : '+match[2]);
+		console.log('Day : '+match[3]);
+	}
+
+	parseIsoDate('2016-07-02');
 }());
