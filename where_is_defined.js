12c277d3de3bee3f887c0ab924bca316e6e29176
Ali-Amechghal <ali.amechghal@gmail.com>
2016-07-30 18:30:02 +0100

    ...

--- where_is_defined.js
+++ where_is_defined.js
@@ -2,10 +2,11 @@
  * check where a given property  is defined in prototype chain
  *
  * */
-
+console.time('profile');
 function getDefiningObject(obj, propKey){
      obj =  Object(obj);//make sure it an object
-     while(obj && ! {}.hasOwnProperty.call(obj, propKey)){
+     var func={}.hasOwnProperty;
+     while(obj && ! func.call(obj, propKey)){
          obj = Object.getPrototypeOf(obj);
          // obj is null if we have reached the end
      }
@@ -17,8 +18,10 @@ function getDefiningObject(obj, propKey){
 
 var  master = {name:'Master', isMaster:true};
 var  slave  = Object.create(master , {
-    name: {value:'salve', writable:true}
+    name: {value:'slave', writable:true}
     });
 
 if(master === getDefiningObject(slave ,  'isMaster'))
     console.log('OK ;  isMaster in Master Object');
+
+console.timeEnd('profile');
